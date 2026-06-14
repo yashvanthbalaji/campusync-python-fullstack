fb8c7d22cdf34b740582ab95f24057766694f0ef
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2026-06-14 21:01:44 +0530

    feat(infra): configure database initialization script and multi-container docker compose orchestration

---
+++ init.sql
@@ -0,0 +1,7 @@
+-- HostelHub Database Initialization
+-- Runs automatically when PostgreSQL container first starts
+
+CREATE DATABASE hostelhub_auth;
+CREATE DATABASE hostelhub_complaint;
+CREATE DATABASE hostelhub_lostfound;
+CREATE DATABASE hostelhub_notifications;
